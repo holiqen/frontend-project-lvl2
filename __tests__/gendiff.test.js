32a5ab4f7cab5d8b04ed1802eda38070cd60df47
holiqen <hekill93@mail.ru>
2020-06-02 18:22:08 +0300

    Update Test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,22 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../src';
-// import after from './__fixtures__/after.json';
-// import before from './__fixtures__/before.json';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const fullPathBefore = path.resolve(`${dirname}/__fixtures__/`, 'before.json');
+const fullPathAfter = path.resolve(`${dirname}/__fixtures__/`, 'after.json');
 
 const expected = `{
   host: hexlet.io
 + timeout: 20
 - timeout: 50
 - proxy: 123.234.53.22
-+ verbose: true
 - follow: false
++ verbose: true
 }`;
 
 test('genDiff', () => {
-  expect(genDiff('./__fixtures__/after.json', './__fixtures__/before.json')).toBe(expected);
+  expect(genDiff(fullPathBefore, fullPathAfter)).toBe(expected);
 });
